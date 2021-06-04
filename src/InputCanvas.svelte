e131fdeaca3f83b34186476e348ef69083175694
Bogdan740 <bogdancuciureanup@gmail.com>
2021-06-04 19:30:53 +0100

    Scaled stroke weight to size of canvas

--- src/InputCanvas.svelte
+++ src/InputCanvas.svelte
@@ -8,11 +8,10 @@
 
 <P5Canvas
   setup={(p) => {
-    p.createCanvas(280, 280); // Resize canvas here @HAM
+    p.createCanvas(280,280); // Resize canvas here @HAM
     smallCanvas = p.createGraphics(28,28);
     p.pixelDensity(1);
     smallCanvas.pixelDensity(1);
-    smallCanvas.background(0);
     p.background(0);
   }}
   draw={(p) => {
@@ -29,10 +28,10 @@
       pixelShades = [];
       
       p.stroke(255);
-      p.strokeWeight(20);
+      p.strokeWeight(p.height/15);
       p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
       smallCanvas.stroke(255);
-      smallCanvas.strokeWeight(20);
+      smallCanvas.strokeWeight(p.height/15);
       smallCanvas.line(p.mouseX,p.mouseY,p.pmouseX,p.pmouseY)
       smallCanvas.image(p,0,0,28,28)
     }
