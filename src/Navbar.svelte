6684f0dc62552ace4032b6904ee6b18aa524d773
Hamzah Qureshi <hamzahqureshi25@gmail.com>
2021-06-04 21:53:13 +0100

    Added non functional navbar

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -2,15 +2,24 @@
     import {MDBNavbar} from "mdbsvelte";
 </script>
 
-<MDBNavbar class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-    <button
-    class="navbar-toggler"
-    type="button"
-    data-mdb-toggle="collapse"
-    data-mdb-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent"
-    aria-expanded="false"
-    aria-label="Toggle navigation">
-        About
-    </button>
-</MDBNavbar>
+<nav class="navbar navbar-expand-xxl fixed-top navbar-dark bg-dark" role="navigation">
+    <div class="container-fluid">
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarToggleExternalContent"
+        aria-controls="navbarToggleExternalContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarToggleExternalContent">
+            <span class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sem nisi, ornare ut dui vel, fringilla interdum leo. Mauris ipsum augue, aliquet a libero non, lobortis pharetra est. Proin scelerisque tempus fermentum. Sed vel bibendum lacus. Nulla vitae ipsum nulla. Etiam fermentum dapibus nulla sed mollis. Donec vulputate libero urna, sit amet tincidunt eros sollicitudin sed. Sed mauris arcu, molestie sit amet ultrices quis, aliquam non sapien. Nullam at justo velit. Donec maximus neque aliquet purus tristique vestibulum. Nullam sit amet posuere sapien. Vestibulum luctus non mi non lacinia.
+            </span>
+        </div>
+    </div>
+</nav>
